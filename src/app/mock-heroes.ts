fd1fb34767fce38795767be74c49a80278bdf706
anupama8890 <AnupamaS@SycaSol.com>
2018-07-19 17:52:26 +0530

    chages

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,6 +2,7 @@ import { Item } from './hero';
 
 export const ITEMS: Item[] =
  [
-    {id:1, name:"Item1", item_code:"101", description:"Item 1" , item_image:"/assets/item1.jpeg", item_price:100, quantity:0},
-    {id:2, name:"Item2", item_code:"102", description:"Item 2" , item_image:"/assets/item2.jpeg", item_price:50, quantity:0}
+    {id:1, name:"Item1", item_code:"101", description:"Item 1" , item_image:"/assets/f-shirt-blue.jpg", item_price:100, quantity:0},
+    {id:2, name:"Item2", item_code:"102", description:"Item 2" , item_image:"/assets/f-shirt-cyan.jpg", item_price:50, quantity:0},
+    {id:3, name:"Item2", item_code:"102", description:"Item 2" , item_image:"/assets/f-shirt-green.jpg", item_price:50, quantity:0}
 ];
